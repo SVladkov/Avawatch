a41cc20d8a4d8498e4ca365699a7e2fc4410bf92
svladkov <s.v.vladkov@gmail.com>
2018-03-07 23:07:37 +0200

    Set CronJob for the scrapers

--- Scrapers/index.js
+++ Scrapers/index.js
@@ -1,11 +1,14 @@
 const fetch = require('node-fetch');
 const PssScraper = require('./Scrapers/pssScraper');
 const PssPoster = require('./Posters/pssPoster');
+const CronJob = require('cron').CronJob;
 
 var pssScraper = new PssScraper();
 var pssPoster = new PssPoster();
 
-var forecasts = pssScraper.scrape()
-    .then(forecasts => {
-        pssPoster.postForecasts(forecasts);
-    });
+new CronJob('0 */3 * * * *', () => {
+    pssScraper.scrape()
+        .then(forecasts => {
+            pssPoster.postForecasts(forecasts);
+        });
+}, null, true);
