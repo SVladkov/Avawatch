f3ce36155931668cb7acc48f6bc3651b1c62ccd5
svladkov <s.v.vladkov@gmail.com>
2018-03-12 10:12:46 +0200

    Implemented returing of only the latest avalanche data

--- Persistence/dataAccess/dataAccess.js
+++ Persistence/dataAccess/dataAccess.js
@@ -89,7 +89,16 @@ class dataAccess {
     }
 
     getAllForecasts() {
-        var getAllForecasts = "SELECT date, dangerLevel, regionId, name AS sourceName, url AS sourceUrl FROM forecasts JOIN dataSources";
+
+        var getAllForecasts = "SELECT date, dangerLevel, regionId, name AS sourceName, url AS sourceUrl " +
+            "FROM dataSources " +
+            "JOIN " +
+                "(SELECT * " +
+                "FROM forecasts as data " +
+                "WHERE date = " +
+                    "(SELECT MAX(date) " +
+                    "FROM forecasts " +
+                    "WHERE regionId=data.regionId)) as LatestDates;";
 
         return new Promise((resolve, reject) => {
             this.db.connection.query(getAllForecasts, (err, result) => {
